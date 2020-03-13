09762f69530b0694c3f3015467d78ae910b039a4
tlamart <t_lamart@hotmail.fr>
2020-03-13 23:30:05 +0100

    first project

---
+++ assets/js/todo.js
@@ -0,0 +1,24 @@
+// Check off specific todos by clicking
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+	$(this).remove()
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+	}
+});
+
+$("#btn_plus").click(function(){
+	$("input[type='text']").fadeToggle()
+});
